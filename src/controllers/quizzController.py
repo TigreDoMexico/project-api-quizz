9ccfda731383a21a5d360982ba791995fad709a9
= <=>
2023-08-24 21:30:20 -0300

    Rota para obter quizz - WIP

--- src/controllers/quizzController.py
+++ src/controllers/quizzController.py
@@ -1,10 +1,15 @@
-from src.models.answer import Answer
-from src.models.quizz import Quizz
+from models.answer import Answer
+from models.quizz import Quizz
 
-def obterQuizz():
-    answer1 = Answer('Elisabeth', True)
-    answer2 = Answer('Martha', False)
-    answer3 = Answer('Mary Jane', False)
+def obterQuizz(quantidade = 10, assunto = ''):
+    lista_quizz = []
+
+    for _ in range(10):
+        answer1 = Answer('Elisabeth', True)
+        answer2 = Answer('Martha', False)
+        answer3 = Answer('Mary Jane', False)
     
-    quizz = Quizz('Quem foi a rainha da Inglaterra', [answer1, answer2, answer3])
-    return quizz
+        quizz = Quizz('Quem foi a rainha da Inglaterra', [answer1, answer2, answer3])
+        lista_quizz.append(quizz)
+    
+    return lista_quizz
--- src/index.py
+++ src/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from controllers.quizzController import obterQuizz
 
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route('/quizzes')
 def getQuizz():
-    returno = obterQuizz()
+    quantidade = request.args.get('quantidade')
+    assunto = request.args.get('assunto')
+
+    returno = obterQuizz(int(quantidade) if quantidade else 10, assunto)
     return jsonify(returno.__dict__)
 
 if __name__ == '__main__':
