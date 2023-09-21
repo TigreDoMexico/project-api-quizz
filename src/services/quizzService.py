8ccc4be444c1bdc93833d9b6d270b11bbed80d62
= <=>
2023-09-20 22:06:05 -0300

    Docker: subindo a imagem e funcionando

--- src/services/quizzService.py
+++ src/services/quizzService.py
@@ -6,10 +6,10 @@ class QuizzService:
     def __init__(self, repository = None) -> None:
         self.repositorio = repository if repository else QuizzRepository()
     
-    def obterQuizzes(self, quantidade:int = 10, assunto:str | None = None):
+    def obterQuizzes(self, quantidade:int = 10, assunto = None):
         return self.repositorio.ObterTodos(quantidade, assunto)
 
-    def salvarQuizz(self, pergunta: str, assunto: str | None, respostas) -> bool:
+    def salvarQuizz(self, pergunta: str, assunto, respostas) -> bool:
         lista_answers = [Answer(resposta[0], resposta[1]) for resposta in respostas]
         total_respostas_certas = sum(answer.Correto for answer in lista_answers)
 
